4b28a489160636f5efcea3f2cf2773e2886fc293
Aadarsha Dumre <adarshadumre45@gmail.com>
2026-05-23 17:35:01 +0545

    add new assignment.js file

---
+++ models/assignmentModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const assignmentSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    subject:{
+        type:String,
+        required:true
+    },
+    course:{
+        type:String,
+        required:true
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    description:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+}
+)
+
+const Assignment = mongoose.model("Assignment",assignmentSchema)
