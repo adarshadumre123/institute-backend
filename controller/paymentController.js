a3403932f8bb7c5c512c1128997a884b49ef8847
Aadarsha Dumre <adarshadumre45@gmail.com>
2026-06-24 14:47:39 +0545

    modified files

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -2,52 +2,85 @@ import coursesModel from '../models/coursesModel.js'
 import Course from '../models/coursesModel.js'
 import Enrollment from '../models/enrollmentModel.js'
 import Payment from '../models/paymentModel.js'
+import { generateEsewaSignature } from '../utils/esewaSignature.js'
 import { generateTransactionId } from '../utils/generateTransactionId.js'
 
 
 
-export const createPayment = async(req,res)=>{
+export const createPayment = async (req, res) => {
     try {
-        const{courseId}=req.body;
+        const { courseId } = req.body;
         const course = await Course.findById(courseId)
-        if(!course){
+        if (!course) {
             return res.status(404).json({
-                success:false,
-                message:"course not found"
+                success: false,
+                message: "course not found"
             })
         }
         const enrolled = await Enrollment.findOne({
-            student:req.user.id,
-            course:courseId
+            student: req.user.id,
+            course: courseId
         })
-        if(enrolled){
+        if (enrolled) {
             return res.status(400).json({
-                success:false,
-                message:"already enrolled"
+                success: false,
+                message: "already enrolled"
             })
         }
 
         const transactionId = generateTransactionId()
 
         const payment = await Payment.create({
-            student:req.user.id,
-            course:courseId,
-            amount:course.price,
+            student: req.user.id,
+            course: courseId,
+            amount: course.price,
             transactionId,
-            paymentMethod:"eSewa",
-            status:'pending',
+            paymentMethod: "eSewa",
+            status: 'pending',
         })
 
+        const productCode = process.env.ESEWA_PRODUCT_CODE;
+
+        const signature = generateEsewaSignature(
+            course.price,
+            transactionId,
+            productCode
+        );
+
+
+
         return res.status(201).json({
-            success:true,
-            message:"payment created successfully",
-            payment,
+            success: true,
+            paymentData: {
+                amount: course.price,
+                tax_amount: 0,
+                total_amount: course.price,
+
+                transaction_uuid: transactionId,
+
+                product_code: productCode,
+
+                product_service_charge: 0,
+                product_delivery_charge: 0,
+
+                success_url:
+                    "http://localhost:8000/api/v1/payment/verify-payment",
+
+                failure_url:
+                    "http://localhost:5173/payment-failed",
+
+                signed_field_names:
+                    "total_amount,transaction_uuid,product_code",
+
+                signature,
+            },
+
         })
     } catch (error) {
         return res.status(500).json({
-      success: false,
-      message: error.message,
-    });
+            success: false,
+            message: error.message,
+        });
 
     }
 }
---
+++ utils/esewaSignature.js
@@ -0,0 +1,15 @@
+import crypto from 'crypto'
+
+export const generateEsewaSignature =(
+    totalAmount,
+    transactionUuid,
+    productCode
+)=>{
+    const secretKey = process.env.ESEWA_SECRET_KEY
+    const message=`total_amount=${totalAmount}, transaction_uuid=${transactionUuid}, product code = ${productCode}`
+    const signature = crypto
+    .createHmac("sha256",secretKey)
+    .update(message)
+    .digest('base64')
+    return signature
+}
