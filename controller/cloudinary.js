153b76c5bf5bed6d8d702da07d2739aa87834886
Aadarsha Dumre <adarshadumre45@gmail.com>
2026-07-12 21:59:17 +0545

    modified file

--- controller/cloudinary.js
+++ controller/cloudinary.js
@@ -1,50 +1,61 @@
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs'
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 
 });
 
+console.log({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret_length: process.env.CLOUDINARY_API_SECRET?.length,
+});
+
+console.log(cloudinary.config());
+
 
 cloudinary.api
-  .ping()
-  .then((res) => console.log("PING:", res))
-  .catch((err) => console.log("PING ERROR:", err));
+    .ping()
+    .then((res) => console.log("PING:", res))
+    .catch((err) => console.log("PING ERROR:", err));
 
-
-
-const uploadOnCloudinary=async(localFilePath)=>{
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath){
+        if (!localFilePath) {
             return null
         }
 
-         console.log("Path:", localFilePath);
-    console.log(
-      "File exists:",
-      fs.existsSync(localFilePath)
-    );
+        console.log("Path:", localFilePath);
+        console.log(
+            "File exists:",
+            fs.existsSync(localFilePath)
+        );
 
-        const res =await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"raw"
-        })
-        fs.unlinkSync(localFilePath);
+        const res = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "raw",
+            folder: "test",
+        });
 
-        console.log("file is upload on cloudinay",res.url)
+        console.log(res)
+
         return res
     } catch (error) {
-            console.log("Cloudinary Error:", error);
+        console.log("Cloudinary Error:", error);
+        console.log(error.message);
+        console.log(error.http_code);
+        console.log(error.error);
+        console.log(error.response);
 
-     console.dir(error, { depth: null });
+        console.dir(error, { depth: null });
 
-  if (localFilePath && fs.existsSync(localFilePath)) {
-    fs.unlinkSync(localFilePath);
-  }
+        if (localFilePath && fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath);
+        }
 
-  return null;
+        return null;
     }
 }
 
